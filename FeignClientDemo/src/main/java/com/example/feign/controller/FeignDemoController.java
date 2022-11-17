6be6bdc2f76d5b014133592f941e11f2d3bd205b
SaiAshish <43849911+SaiAshish9@users.noreply.github.com>
2022-11-17 15:14:55 +0530

    Add files via upload

---
+++ FeignClientDemo/src/main/java/com/example/feign/controller/FeignDemoController.java
@@ -0,0 +1,31 @@
+package com.example.feign.controller;
+
+import com.example.feign.util.FeignServiceUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class FeignDemoController {
+
+    @Autowired
+    private FeignServiceUtil feignServiceUtil;
+
+    @GetMapping("/user-name")
+    public String getUserName(){
+        return feignServiceUtil.getName();
+    }
+
+    @GetMapping("/user-address")
+    public String getUserAdd(){
+        return feignServiceUtil.getAddress();
+    }
+
+    @GetMapping("/user-status")
+    public String getUserStatus(){
+        return feignServiceUtil.getStatus();
+    }
+
+}
